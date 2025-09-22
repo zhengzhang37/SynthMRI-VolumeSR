be4129a8802d7c863785260a75a4d4d7040988fc
zhengzhang <37zhangz@gmail.com>
2025-09-22 20:16:45 +0100

    Update inference.py

--- inference.py
+++ inference.py
@@ -10,17 +10,16 @@ import SimpleITK as sitk
 
 def split_image_d_only(imgtensor, crop_size=128, overlap_size=10):
     _, C, D, H, W = imgtensor.shape
-    # 计算在D维上的切割起始位置
     dstarts = [x for x in range(0, D, crop_size - overlap_size)]
     while dstarts[-1] + crop_size > D:
         dstarts.pop()
-    dstarts.append(max(D - crop_size, 0))  # 确保至少有一个完整的切片
+    dstarts.append(max(D - crop_size, 0))  
 
-    # 切割图像，并存储每个切片及其在D维上的起始位置
+
     split_data = []
     starts = []
     for ds in dstarts:
-        cimgdata = imgtensor[:, :, ds:ds + crop_size, :, :]  # 切割操作
+        cimgdata = imgtensor[:, :, ds:ds + crop_size, :, :]  
         starts.append(ds)
         split_data.append(cimgdata)
     return split_data, starts
@@ -29,7 +28,7 @@ def get_scoremap_d_only(D, B=1, C=1):
     center_d = D / 2
     score = torch.ones((B, C, D))
     for d in range(D):
-        score[:, :, d] = 1.0 / (abs(d - center_d) + 1e-3)  # 使用距离中心的倒数作为权重，避免除零错误
+        score[:, :, d] = 1.0 / (abs(d - center_d) + 1e-3)  
     return score
 
 def merge_image_d_only(split_data, starts, crop_size=128, resolution=(1, 1, 500, 256, 256)):
@@ -37,7 +36,7 @@ def merge_image_d_only(split_data, starts, crop_size=128, resolution=(1, 1, 500,
     tot_score = torch.zeros((B, C, D, H, W))
     merge_img = torch.zeros((B, C, D, H, W))
     for simg, ds in zip(split_data, starts):
-        scoremap = get_scoremap_d_only(crop_size, B, C).unsqueeze(-1).unsqueeze(-1)  # 调整形状以匹配H和W维
+        scoremap = get_scoremap_d_only(crop_size, B, C).unsqueeze(-1).unsqueeze(-1)  
         merge_img[:, :, ds:ds + crop_size, :, :] += scoremap * simg[:,:,:D,:H,:W]
         tot_score[:, :, ds:ds + crop_size, :, :] += scoremap
     merge_img = merge_img / tot_score
@@ -66,21 +65,21 @@ def inference(opt, epoch=None):
             # replace file path with your own
             flair_lr_nii_img = sitk.GetArrayFromImage(sitk.ReadImage(os.path.join('.../LR-FLAIR', input_file)))
             flair_hr_nii_img = sitk.GetArrayFromImage(sitk.ReadImage(os.path.join('.../HR-FLAIR', input_file)))
-            tof_hr_nii_img = sitk.GetArrayFromImage(sitk.ReadImage(os.path.join('.../HR-TOF', input_file)))
+            t1_hr_nii_img = sitk.GetArrayFromImage(sitk.ReadImage(os.path.join('.../HR-T1', input_file)))
 
             max_flair_lr = flair_lr_nii_img.max().astype(np.float32)
             max_flair_hr = flair_hr_nii_img.max().astype(np.float32)
-            max_tof_hr = tof_hr_nii_img.max().astype(np.float32)
+            max_t1_hr = t1_hr_nii_img.max().astype(np.float32)
 
             flair_lr = preprocess(flair_lr_nii_img, max_flair_lr)
             flair_hr = preprocess(flair_hr_nii_img, max_flair_hr)
-            tof_hr = preprocess(tof_hr_nii_img, max_tof_hr)
+            t1_hr = preprocess(t1_hr_nii_img, max_t1_hr)
 
             flair_lr = torch.from_numpy(flair_lr).unsqueeze(0).unsqueeze(0).cuda()
             flair_hr = torch.from_numpy(flair_hr).unsqueeze(0).unsqueeze(0).cuda()
-            tof_hr = torch.from_numpy(tof_hr).unsqueeze(0).unsqueeze(0).cuda()
+            t1_hr = torch.from_numpy(t1_hr).unsqueeze(0).unsqueeze(0).cuda()
 
-            input_image, target_image = torch.cat((flair_lr, tof_hr), dim=1), tof_hr
+            input_image, target_image = torch.cat((flair_lr, t1_hr), dim=1), t1_hr
 
             output = model(input_image)
 
